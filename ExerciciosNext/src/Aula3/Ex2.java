052a3ac860a0d27ebb37a89589430663f73f9663
Hugo Oliveira <hugooli2412@gmail.com>
2022-05-11 17:13:31 -0300

    Exercícios 1 e 2 da aula 3 do Next

---
+++ ExerciciosNext/src/Aula3/Ex2.java
@@ -0,0 +1,17 @@
+package Aula3;
+
+import java.util.Arrays;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        int x, y, z;
+        int[] inteiros = new int[3];
+        x = 1;
+        y = 2;
+        z = 3;
+
+        inteiros[0] = x;
+        inteiros[1] = y;
+        inteiros[2] = z;
+    }
+}
