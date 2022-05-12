63c373045d2f7d3593bb273209504f4d5f7f51ce
Oligonautaa <Oligonauta@gmail.com>
2022-05-12 12:44:02 -0300

    Adicionando desafio métodos com a classe Data

--- ExerciciosUdemy/Secao5/src/DesafioClasseData/Data.java
+++ ExerciciosUdemy/Secao5/src/DesafioClasseData/Data.java
@@ -1,6 +1,8 @@
 package DesafioClasseData;
-//Crie uma classe data com os atributos dia, mes e ano.
-//Além disso, instancie numa classe DataTeste 2 objetos da classe Data.
+/*1.Crie uma classe data com os atributos dia, mes e ano.
+    Além disso, instancie numa classe DataTeste 2 objetos da classe Data.
+  2.Crie um método que retorne a data completa.
+*/
 public class Data {
     int dia, mes, ano;
 
@@ -8,5 +10,12 @@ public class Data {
         this.dia = dia;
         this.mes = mes;
         this.ano = ano;
+    }
+    String formatarData(){
+       return String.format("%d/%d/%d", dia, mes, ano);
+    }
+
+    void imprimirData(){
+        System.out.println(formatarData());
     }
 }
