510ea20a58e3ed82c59be83658297dd5a445020a
Oligonautaa <Oligonauta@gmail.com>
2022-05-12 10:30:40 -0300

    Inserindo comentários na classe Data

--- ExerciciosUdemy/Secao5/src/DesafioClasseData/Data.java
+++ ExerciciosUdemy/Secao5/src/DesafioClasseData/Data.java
@@ -1,5 +1,6 @@
 package DesafioClasseData;
-
+//Crie uma classe data com os atributos dia, mes e ano.
+//Além disso, instancie numa classe DataTeste 2 objetos da classe Data.
 public class Data {
     int dia, mes, ano;
 
