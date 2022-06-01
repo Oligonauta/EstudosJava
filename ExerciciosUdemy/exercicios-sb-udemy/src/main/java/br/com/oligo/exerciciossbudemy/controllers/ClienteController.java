b172fef2c5ca39784fa73e3df28270e99ecba583
Oligonautaa <Oligonauta@gmail.com>
2022-05-31 22:43:29 -0300

    Mapeamento de requisições e interação padrâmetros URL

--- ExerciciosUdemy/exercicios-sb-udemy/src/main/java/br/com/oligo/exerciciossbudemy/controllers/ClienteController.java
+++ ExerciciosUdemy/exercicios-sb-udemy/src/main/java/br/com/oligo/exerciciossbudemy/controllers/ClienteController.java
@@ -1,8 +1,6 @@
 package br.com.oligo.exerciciossbudemy.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.oligo.exerciciossbudemy.models.Cliente;
 
@@ -12,5 +10,14 @@ public class ClienteController {
     @GetMapping("/qualquer")
     public Cliente obterCliente() {
         return new Cliente(1, "Hugo", "123.456.789-00");
+    }
+    @GetMapping("/{id}")
+    public Cliente obterClientePorId1(@PathVariable int id){
+        return new Cliente(id, "Duda", "987.654.321-00");
+    }
+    @GetMapping
+    public Cliente obterClientePorId2(
+            @RequestParam(name ="id") int id){
+        return new Cliente(id, "Isa", "111.222.333-44");
     }
 }
